b8a4efbfc3e7bf3d81f872a5c6204d5b238498e9
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-10-01 09:49:59 +0300

    Part 12 Exercise 7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,17 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const updatedToDo = await Todo.findByIdAndUpdate(
+    req.todo._id,
+    req.body,
+    { new: true, runValidators: true }
+  )
+  res.json(updatedToDo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
