b77c1451a519bfe05f9fc0342c73f54b1929b512
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-10-07 12:43:54 +0300

    Part 12 Exercise 18

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
 const { MONGO_URL } = require('../util/config')
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+if (MONGO_URL && !mongoose.connection.readyState) {
+  console.log(MONGO_URL)
+  try {
+    mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log('connected')
+  } catch (e) {
+    console.log(e)
+  }
+  
+}
 
 
 module.exports = {
